7fcbe823b49470ea99dc364f7006d72199c09528
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2022-11-16 20:33:43 +0000

    svelte_head is no more required to import css

--- src/App.svelte
+++ src/App.svelte
@@ -1,16 +1,19 @@
-<!-- App.svelte -->
+<style>
+    @import "carbon-components-svelte/css/all.css"; /* in case it does not load, refer to [1^SOLUTION] */
+</style>
+
 <script>
-    // import "./carbon-components-svelte/css/all.css"; /* [ISSUE] : SOMEWHY DOES NOT LOAD ; please refer to [1^SOLUTION] */
     import { Button } from "carbon-components-svelte";
 </script>
 
-<!-- [1^SOLUTION] : link style through svelte:head -->
-<svelte:head>
+<!-- [1^SOLUTION] : link style through svelte:head tags -->
+<!-- <svelte:head>
     <link
         rel="stylesheet"
         href="./carbon-components-svelte/css/all.css"
     />
-</svelte:head>
+</svelte:head> -->
 
 <Button kind="primary">CARBON BY IBM</Button>
+<Button kind="secondary">CARBON BY IBM</Button>
 
