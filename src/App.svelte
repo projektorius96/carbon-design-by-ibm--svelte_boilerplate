17824e0cc58bdd0085f983cddf0ab11467c55dd2
Lukas Gaucas <34185919+projektorius96@users.noreply.github.com>
2022-12-23 11:37:55 +0000

    proper css import from node_modules

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,14 @@
 <style>
-    @import "carbon-components-svelte/css/all.css"; /* in case it does not load, refer to [1^SOLUTION] */
+    /* @import "carbon-components-svelte/css/all.css"; */ /* in case it does not load, refer to [1^SOLUTION] */
 </style>
 
 <script>
+    import { onMount } from 'svelte';
     import { Button } from "carbon-components-svelte";
+
+    onMount(async()=>{
+        await import("carbon-components-svelte/css/white.css")
+    })
 </script>
 
 <!-- [1^SOLUTION] : link style through svelte:head tags -->
